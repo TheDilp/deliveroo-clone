89cad377863be330964f37b77b3e29e8215db4b6
Arctur <mihailo.dilparic@gmail.com>
2022-07-10 13:36:09 +0200

    Init commit

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,37 @@
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from "react-native-heroicons";
+export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="mt-7">
+      {/* Header */}
+
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="w-7 h-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View>
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location <ChevronDownIcon size={20} color="#00ccbb" />
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
