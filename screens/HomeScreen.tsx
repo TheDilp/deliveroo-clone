4e9380c3655779f674fc9942812daa1f0fc715e7
Arctur <mihailo.dilparic@gmail.com>
2022-07-10 13:54:49 +0200

    Search bar added

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,12 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
-  UserIcon,
   ChevronDownIcon,
+  UserIcon,
   SearchIcon,
   AdjustmentsIcon,
-} from "react-native-heroicons";
+} from "react-native-heroicons/outline";
 export default function HomeScreen() {
   const navigation = useNavigation();
 
@@ -17,7 +17,7 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <SafeAreaView className="mt-7">
+    <SafeAreaView className="mt-7 bg-white ">
       {/* Header */}
 
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
@@ -25,12 +25,25 @@ export default function HomeScreen() {
           source={{ uri: "https://links.papareact.com/wru" }}
           className="w-7 h-7 bg-gray-300 p-4 rounded-full"
         />
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
           <Text className="font-bold text-xl">
             Current Location <ChevronDownIcon size={20} color="#00ccbb" />
           </Text>
         </View>
+        <UserIcon size={35} color="#00ccbb" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row items-center flex-1 space-x-2 bg-gray-200 p-3 mr-4">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsIcon color="#00ccbb" />
       </View>
     </SafeAreaView>
   );
