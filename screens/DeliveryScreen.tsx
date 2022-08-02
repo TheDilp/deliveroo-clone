f5364b126e0b2afe0318f16ef269d62bffc9547b
Arctur <mihailo.dilparic@gmail.com>
2022-08-02 12:31:01 +0200

    Added "driver" info to bottom of delivery screen and contact

--- screens/DeliveryScreen.tsx
+++ screens/DeliveryScreen.tsx
@@ -6,7 +6,8 @@ import { selectRestaurant } from "../features/restaurantSlice";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { XIcon } from "react-native-heroicons/solid";
 import { Bar } from "react-native-progress";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import { openURL } from "expo-linking";
 
 export default function DeliveryScreen() {
   const navigation = useNavigation();
@@ -39,20 +40,50 @@ export default function DeliveryScreen() {
             color="#00ccbb"
             indeterminate={true}
           />
+          <Text className="mt-3 text-center text-xs text-gray-500">
+            Your order at {restaurant?.name} is being prepared
+          </Text>
         </View>
       </SafeAreaView>
-      <View>
-        <MapView
-          initialRegion={{
+      <MapView
+        initialRegion={{
+          latitude: restaurant?.lat || 0,
+          longitude: restaurant?.long || 0,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        className="flex-1 w-full z-0 -mt-10"
+        mapType="mutedStandard"
+      >
+        <Marker
+          title={restaurant?.name}
+          coordinate={{
             latitude: restaurant?.lat || 0,
             longitude: restaurant?.long || 0,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
           }}
-          className="flex-1 w-full h-full z-0 -mt-10"
-          mapType="mutedStandard"
-        ></MapView>
-      </View>
+          pinColor="#00ccbb"
+        />
+      </MapView>
+      <SafeAreaView className="bg-white flex-row items-top space-x-6 h-28">
+        <Image
+          source={require("../assets/drivericon.jpeg")}
+          className="w-12 h-12 bg-gray-300 p4 rounded-full ml-5"
+        />
+        <View className="flex-1">
+          <Text className="text-lg">Mihailo Dilparić</Text>
+          <Text className="text-gray-400 -mt-2">Your developer</Text>
+        </View>
+        <Text
+          className="text-[#00ccbb] text-lg mr-5 font-bold"
+          onPress={() =>
+            openURL(
+              "https://www.linkedin.com/in/mihailo-dilpari%C4%87-856a01241/"
+            )
+          }
+        >
+          Contact
+        </Text>
+      </SafeAreaView>
     </View>
   );
 }
